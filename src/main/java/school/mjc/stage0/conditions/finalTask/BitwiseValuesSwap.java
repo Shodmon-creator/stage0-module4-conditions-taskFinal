52781fd0d802bac47a36a31996f359295a79ec45
Shodmon-creator <mamayusupovshodmon2003@gmail.com>
2022-11-01 23:40:15 +0500

    mistakes have been corrected

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
     public void swap(int first, int second) {
-        second=first|second;
+        second=first+second;
         first=second-first;
         second=second-first;
         System.out.println(first);
